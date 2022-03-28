a360bc3a6ae4fc089f01f4adce9d8e8d64eef868
MateuszKom <101513370+MateuszKom@users.noreply.github.com>
2022-03-28 20:36:21 +0200

    Add files via upload

---
+++ VsGaam/VsGaam/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VsGaam
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public string Publisher { get; set; }
+        public string Developer { get; set; }
+        public string Category { get; set; }
+        public int Music { get; set; }
+        public int Audio { get; set; }
+        public int Performance { get; set; }
+        public int Visuals { get; set; }
+        public string IsMultiPlayerMode { get; set; }
+    }
+}
